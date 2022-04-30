ecb0da672c22b4ead30c17916a1626d6f57dd846
Julius Gvozdiovas <github@julgvoz.com>
2022-04-30 15:00:40 +0200

    Basic static page and api

---
+++ src/main/java/travelitinerary/api/GreetingController.java
@@ -0,0 +1,19 @@
+package travelitinerary.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping("")
+    public ResponseEntity<String> greeting(
+            @RequestParam(name="name", required = false, defaultValue = "World") String name
+            ) {
+        return ResponseEntity.ok("Hello, " + name + "!");
+    }
+}
