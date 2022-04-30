795376bb9fc28dafa2759c4b01165996c4b049ca
Simeon Atanasov <S.Atanasov@student.tudelft.nl>
2022-04-30 23:41:38 +0200

    Made results section scrollable

--- src/main/resources/static/javascripts/index.js
+++ src/main/resources/static/javascripts/index.js
@@ -12,7 +12,18 @@ function requestChange(){
 function getDemoEntry(){
     let entry = document.createElement("div");
     entry.classList.add("entry")
-    entry.innerHTML = "";
+    entry.innerHTML = `<h3>Barcelona, Spain</h3>
+    <span class="date_start">21 May</span>
+    <span class="date_end">4 June</span>
+    <span class="number_of_days">14 days</span>
+    <ul>
+        <li>sight 1</li>
+        <li>sight 2</li>
+        <li>...and more</li>
+    </ul>
+    <img style="background-color: blue; width:2em; height:2em;">
+    <button>More info</button>
+    <span class="price">1473.00</span>`;
     return entry;
 }
 
