e732eba61223464692068f2ad7f1656e86e432cb
Julius Gvozdiovas <github@julgvoz.com>
2022-05-01 10:19:01 +0200

    Load airports

--- src/main/java/entities/airports/Airport.java
+++ src/main/java/entities/airports/Airport.java
@@ -1,5 +1,7 @@
 package entities.airports;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
--- src/main/java/travelitinerary/services/AirportService.java
+++ src/main/java/travelitinerary/services/AirportService.java
@@ -4,6 +4,8 @@ import entities.airports.Airport;
 import org.springframework.stereotype.Service;
 import travelitinerary.repositories.AirportRepository;
 
+import java.util.List;
+
 @Service
 public class AirportService {
     private final AirportRepository airportRepository;
@@ -14,5 +16,21 @@ public class AirportService {
 
     public Airport getByIATA(String iata) {
         return airportRepository.getById(iata);
+    }
+
+    public Airport getByCityName(String cityName) {
+        return airportRepository.findByCityName(cityName);
+    }
+
+    public void clear() {
+        airportRepository.deleteAll();
+    }
+
+    public List<Airport> save(List<Airport> airports) {
+        return airportRepository.saveAll(airports);
+    }
+
+    public int count() {
+        return (int) airportRepository.count();
     }
 }
